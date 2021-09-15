7d3f4ef75a1e38af1881974bd5d226adb2ce972a
Pablo Santiago <pablosantiagolopez1@gmail.com>
2021-09-15 16:37:22 +0200

    Project structure initialization

---
+++ rekono/executions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ExecutionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'executions'
---
+++ rekono/findings/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FindingsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'findings'
