a03fa8237265d9c92cb51b83690fcabdeff3ac27
Pablo Santiago <pablosantiagolopez1@gmail.com>
2021-09-15 17:14:49 +0200

    Users app: model

--- rekono/users/admin.py
+++ rekono/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from users.models import UserDetail
 
 # Register your models here.
+
+admin.site.register(UserDetail)
--- rekono/users/models.py
+++ rekono/users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class UserDetail(models.Model):
+
+    class Notification(models.IntegerChoices):
+        MAIL = 1
+        TELEGRAM = 2
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    notification_preference = models.IntegerField(
+        choices=Notification.choices,
+        default=Notification.MAIL,
+        blank=True,
+        null=True
+    )
+    telegram_token = models.TextField(max_length=50, blank=True, null=True)
