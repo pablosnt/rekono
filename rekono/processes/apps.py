4bba7c215e23e32fd9adbb425371e13091872203
Pablo Santiago <pablosantiagolopez1@gmail.com>
2021-09-15 17:13:05 +0200

    Processes app: model

--- rekono/processes/apps.py
+++ rekono/processes/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class ProcessesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'processes'
--- rekono/processes/models.py
+++ rekono/processes/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tools.models import Tool, Configuration
 
 # Create your models here.
+
+
+class Process(models.Model):
+    name = models.TextField(max_length=30)
+    description = models.TextField(max_length=250)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Step(models.Model):
+
+    class Priority(models.IntegerChoices):
+        LOW = 1
+        MEDIUM = 2
+        HIGH = 3
+
+    process = models.ForeignKey(Process, on_delete=models.CASCADE)
+    tool = models.ForeignKey(Tool, on_delete=models.CASCADE)
+    configuration = models.ForeignKey(
+        Configuration,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    priority = models.IntegerField(
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
+
+    def __str__(self) -> str:
+        return self.process.__str__() + ' - ' + self.configuration.__str__()
