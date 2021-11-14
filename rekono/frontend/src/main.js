20de57bd391a30d6b36751b51cb870072014ee99
Pablo Santiago <pablosantiagolopez1@gmail.com>
2021-11-14 17:19:22 +0100

    Initial login page

--- rekono/frontend/src/main.js
+++ rekono/frontend/src/main.js
@@ -4,11 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import BootstrapVue from 'bootstrap-vue'
+import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 /* eslint-disable no-new */
 new Vue({
