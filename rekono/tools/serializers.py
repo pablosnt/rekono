0840e22f5148707dcdc81e53c1ccc466727fb694
Pablo Santiago <pablosantiagolopez1@gmail.com>
2021-12-02 21:10:00 +0100

    Sort correction for related tools entities

--- rekono/tools/serializers.py
+++ rekono/tools/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 from tools.models import Configuration, Input, Intensity, Output, Tool
 
 
@@ -19,13 +20,27 @@ class OutputSerializer(serializers.ModelSerializer):
 
 
 class ConfigurationSerializer(serializers.ModelSerializer):
-    inputs = InputSerializer(read_only=True, many=True, required=False)
-    outputs = OutputSerializer(read_only=True, many=True, required=False)
+    inputs = SerializerMethodField(
+        method_name='get_inputs',
+        read_only=True,
+        required=False
+    )
+    outputs = SerializerMethodField(
+        method_name='get_outputs',
+        read_only=True,
+        required=False
+    )
 
     class Meta:
         model = Configuration
         fields = ('id', 'name', 'tool', 'arguments', 'default', 'inputs', 'outputs')
         ordering = ['-id']
+
+    def get_inputs(self, instance):
+        return InputSerializer(instance.inputs.all().order_by('id'), many=True).data
+
+    def get_outputs(self, instance):
+        return OutputSerializer(instance.outputs.all().order_by('id'), many=True).data
 
 
 class IntensitySerializer(serializers.ModelSerializer):
@@ -37,8 +52,16 @@ class IntensitySerializer(serializers.ModelSerializer):
 
 
 class ToolSerializer(serializers.ModelSerializer):
-    intensities = IntensitySerializer(read_only=True, many=True, required=False)
-    configurations = ConfigurationSerializer(read_only=True, many=True, required=False)
+    intensities = SerializerMethodField(
+        method_name='get_intensities',
+        read_only=True,
+        required=False
+    )
+    configurations = SerializerMethodField(
+        method_name='get_configurations',
+        read_only=True,
+        required=False
+    )
 
     class Meta:
         model = Tool
@@ -46,3 +69,12 @@ class ToolSerializer(serializers.ModelSerializer):
             'id', 'name', 'command', 'stage', 'reference', 'icon',
             'for_each_target_port', 'intensities', 'configurations'
         )
+
+    def get_intensities(self, instance):
+        return IntensitySerializer(instance.intensities.all().order_by('value'), many=True).data
+
+    def get_configurations(self, instance):
+        return ConfigurationSerializer(
+            instance.configurations.all().order_by('-default', 'name'),
+            many=True
+        ).data
