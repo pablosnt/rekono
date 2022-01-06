b245ba6be277b93eb0c55e0d7925722359e2348a
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-01-06 17:17:17 +0100

    New icons in main header dropdown

--- rekono/frontend/src/common/Header.vue
+++ rekono/frontend/src/common/Header.vue
@@ -7,11 +7,20 @@
       <b-navbar-nav class="ml-auto">
         <b-nav-item-dropdown right>
           <template #button-content>
-            <b-icon style="width: 20px; height: 20px;" icon="person-fill" variant="light"/>
+            <b-icon icon="person-fill" variant="light"/>
           </template>
-          <b-dropdown-item @click="$router.push('/profile')">Profile</b-dropdown-item>
-          <b-dropdown-item href="/api/schema/swagger-ui.html?docExpansion=none" target="_blank">Rekono API Rest</b-dropdown-item>
-          <b-dropdown-item @click="handleLogout">Logout</b-dropdown-item>
+          <b-dropdown-item @click="$router.push('/profile')">
+            <b-icon variant="black" icon="person-fill"/>
+            <label class="ml-2">Profile</label>
+          </b-dropdown-item>
+          <b-dropdown-item href="/api/schema/swagger-ui.html?docExpansion=none" target="_blank">
+            <b-icon variant="danger" icon="code-slash"/>
+            <label class="ml-2">Rekono API Rest</label>
+          </b-dropdown-item>
+          <b-dropdown-item @click="handleLogout">
+            <b-icon variant="secondary" icon="box-arrow-right"/>
+            <label class="ml-2">Logout</label>
+          </b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-navbar>
