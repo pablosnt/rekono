c4ccc6cfb27383acbe495b645f8172cda19a3413
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-02-11 19:53:23 +0100

    Unit testing for Resources module

--- rekono/testing/unit/base.py
+++ rekono/testing/unit/base.py
@@ -1,5 +1,6 @@
 import json
-from typing import Any, Callable, Dict
+import os
+from typing import Any, Callable, Dict, List
 
 from django.http import HttpResponse
 from django.test import TestCase
@@ -13,6 +14,7 @@ class RekonoTestCase(TestCase):
     def setUp(self) -> None:
         '''Create initial data before run tests.'''
         super().setUp()
+        self.current_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')         # Testing path
         self.username = 'rekono'                                                # Username for test authentication
         self.email = 'rekono@rekono.rekono'                                     # Email for test authentication
         self.password = 'rekono'                                                # Password for test authentication
@@ -39,7 +41,8 @@ class RekonoTestCase(TestCase):
         endpoint: str,
         expected_status: int,
         data: Dict[str, Any] = {},
-        expected: Dict[str, Any] = {}
+        expected: Dict[str, Any] = {},
+        format: str = 'json'
     ) -> Dict[Any, Any]:
         '''Test Rekono API.
 
@@ -49,12 +52,13 @@ class RekonoTestCase(TestCase):
             expected_status (int): Expected HTTP status code
             data (Dict[str, Any], optional): Request body. Defaults to {}.
             expected (Dict[str, Any], optional): Expected response content. Defaults to {}.
+            format (str, optional): Request body format. Defaults to 'json'.
 
         Returns:
             Dict[Any, Any]: Response content
         '''
         if data:                                                                # HTTP body
-            response = request(endpoint, data=data, format='json')              # Make Rekono API request
+            response = request(endpoint, data=data, format=format)              # Make Rekono API request
         else:                                                                   # No HTTP body
             response = request(endpoint)                                        # Make Rekono API request
         self.assertEqual(response.status_code, expected_status)                 # Check HTTP status code
@@ -63,3 +67,14 @@ class RekonoTestCase(TestCase):
             for field, value in expected.items():                               # For each expected data
                 self.assertEqual(content[field], value)                         # Check expected value for field
         return content
+
+    def check_fields(self, fields: List[str], response: Dict[str, Any], expected: Dict[str, Any]) -> None:
+        '''Check expected values for some response fields.
+
+        Args:
+            fields (List[str]): List of field names
+            response (Dict[str, Any]): Response data
+            expected (Dict[str, Any]): Expected data
+        '''
+        for field in fields:
+            self.assertEqual(response[field], expected[field])
---
+++ rekono/testing/unit/test_resources.py
@@ -0,0 +1,75 @@
+import os
+
+from testing.unit.base import RekonoTestCase
+
+
+class ResourcesTest(RekonoTestCase):
+    '''Test cases for Resources module.'''
+
+    def setUp(self) -> None:
+        '''Create initial data before run tests.'''
+        super().setUp()
+        self.passwords = os.path.join(self.current_path, 'data', 'resources', 'passwords.txt')      # Password wordlists
+        self.endpoints = os.path.join(self.current_path, 'data', 'resources', 'endpoints.txt')      # Endpoint wordlists
+        with open(self.passwords, 'r') as wordlist:
+            self.wordlist = self.api_test(                                      # Create wordlist for testing
+                self.rekono.post, '/api/resources/wordlists/', 201,
+                {'name': 'ZZZ', 'type': 'Password', 'file': wordlist},
+                {'name': 'ZZZ', 'type': 'Password', 'size': 3},
+                'multipart'
+            )
+
+    def test_create_wordlist(self) -> None:
+        '''Test wordlist creation feature.'''
+        content = self.api_test(self.rekono.get, '/api/resources/wordlists/?o=-name', 200)          # Get all wordlists
+        self.check_fields(['name', 'type', 'size', 'size', 'creator'], content['results'][0], self.wordlist)
+        with open(self.endpoints, 'r') as wordlist:
+            data = {'name': 'ZZZZZZ', 'type': 'Endpoint', 'file': wordlist}
+            new_wordlist = self.api_test(                                       # Create new wordlist
+                self.rekono.post, '/api/resources/wordlists/', 201,
+                data, {'name': 'ZZZZZZ', 'type': 'Endpoint', 'size': 3}, 'multipart'
+            )
+            # Wordlist name already exists
+            self.api_test(self.rekono.post, '/api/resources/wordlists/', 400, data, {}, 'multipart')
+        content = self.api_test(self.rekono.get, '/api/resources/wordlists/?o=-name', 200)          # Get all wordlists
+        # Check that the first one is the new wordlist
+        self.check_fields(['name', 'type', 'size', 'size', 'creator'], content['results'][0], new_wordlist)
+
+    def test_update_wordlist(self) -> None:
+        '''Test wordlist update feature.'''
+        with open(self.passwords, 'r') as wordlist:
+            data = {'name': 'ZZZ updated', 'type': 'Password', 'file': wordlist}
+            updated = self.api_test(                                            # Update testing wordlist
+                self.rekono.put, f'/api/resources/wordlists/{self.wordlist["id"]}/', 200,
+                data, {'name': 'ZZZ updated', 'type': 'Password', 'size': 3}, 'multipart'
+            )
+            # Wordlist name already exists
+            self.api_test(
+                self.rekono.put, f'/api/resources/wordlists/{self.wordlist["id"]}/', 400,
+                data, {}, 'multipart'
+            )
+        # Check the updated wordlist data
+        self.api_test(self.rekono.get, f'/api/resources/wordlists/{self.wordlist["id"]}/', 200, {}, updated)
+
+    def test_delete_wordlist(self) -> None:
+        '''Test wordlist deletion feature.'''
+        before = self.api_test(self.rekono.get, '/api/resources/wordlists/?o=-name', 200)           # Get wordlists
+        # Delete testing wordlist
+        self.api_test(self.rekono.delete, f'/api/resources/wordlists/{self.wordlist["id"]}/', 204, {})
+        # Check the wordlist deletion
+        self.api_test(self.rekono.get, '/api/resources/wordlists/?o=-name', 200, {}, {'count': before['count'] - 1})
+
+    def test_like_dislike_wordlist(self) -> None:
+        '''Test like and dislike features for wordlists.'''
+        # Like testing wordlist
+        self.api_test(self.rekono.post, f'/api/resources/wordlists/{self.wordlist["id"]}/like/', 201)
+        self.api_test(                                                          # Check that the wordlist is liked
+            self.rekono.get, f'/api/resources/wordlists/{self.wordlist["id"]}/',
+            200, {}, {'liked': True, 'likes': 1}
+        )
+        # Dislike testing wordlist
+        self.api_test(self.rekono.post, f'/api/resources/wordlists/{self.wordlist["id"]}/dislike/', 204)
+        self.api_test(                                                          # Check that the wordlist is not liked
+            self.rekono.get, f'/api/resources/wordlists/{self.wordlist["id"]}/',
+            200, {}, {'liked': False, 'likes': 0}
+        )
