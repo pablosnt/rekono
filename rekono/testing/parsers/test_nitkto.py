d60901ac787bba0f5e42b9889be269356674a14e
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-02-19 12:32:23 +0100

    Unit testing for Nikto parser

---
+++ rekono/testing/parsers/test_nitkto.py
@@ -0,0 +1,81 @@
+from findings.enums import Severity
+from findings.models import Endpoint, Vulnerability
+from testing.parsers.base import ToolParserTest
+
+
+class NiktoParserTest(ToolParserTest):
+    '''Test cases for Nikto parser.'''
+
+    tool_name = 'Nikto'
+
+    def test_default(self) -> None:
+        '''Test to parse report with some vulnerabilities and endpoints.'''
+        expected = [
+            {
+                'model': Vulnerability,
+                'name': 'The anti-clickjacking X-Frame-Options header is not present.',
+                'description': '[GET /] The anti-clickjacking X-Frame-Options header is not present.',
+                'severity': Severity.MEDIUM,
+                'osvdb': 0
+            },
+            {
+                'model': Vulnerability,
+                'name': 'The X-XSS-Protection header is not defined. This header can hint to the user agent to protect against some forms of XSS',      # noqa: E501
+                'description': '[GET /] The X-XSS-Protection header is not defined. This header can hint to the user agent to protect against some forms of XSS',       # noqa: E501
+                'severity': Severity.MEDIUM,
+                'osvdb': 0
+            },
+            {
+                'model': Vulnerability,
+                'name': 'The X-Content-Type-Options header is not set. This could allow the user agent to render the content of the site in a different fashion to the MIME type',      # noqa: E501
+                'description': '[GET /] The X-Content-Type-Options header is not set. This could allow the user agent to render the content of the site in a different fashion to the MIME type',       # noqa: E501
+                'severity': Severity.MEDIUM,
+                'osvdb': 0
+            },
+            {
+                'model': Vulnerability,
+                'name': 'Uncommon header \'tcn\' found, with contents: list',
+                'description': '[GET /index] Uncommon header \'tcn\' found, with contents: list',
+                'severity': Severity.MEDIUM,
+                'osvdb': 0
+            },
+            {'model': Endpoint, 'endpoint': '/index'},
+            {
+                'model': Vulnerability,
+                'name': 'Apache mod_negotiation is enabled with MultiViews, which allows attackers to easily brute force file names. See http://www.wisec.it/sectou.php?id=4698ebdc59d15. The following alternatives for \'index\' were found: index.html',     # noqa: E501
+                'description': '[GET /index] Apache mod_negotiation is enabled with MultiViews, which allows attackers to easily brute force file names. See http://www.wisec.it/sectou.php?id=4698ebdc59d15. The following alternatives for \'index\' were found: index.html',     # noqa: E501
+                'severity': Severity.MEDIUM,
+                'osvdb': 0
+            },
+            {
+                'model': Vulnerability,
+                'name': 'Apache/2.4.7 appears to be outdated (current is at least Apache/2.4.37). Apache 2.2.34 is the EOL for the 2.x branch.',        # noqa: E501
+                'description': '[HEAD /] Apache/2.4.7 appears to be outdated (current is at least Apache/2.4.37). Apache 2.2.34 is the EOL for the 2.x branch.',    # noqa: E501
+                'severity': Severity.MEDIUM,
+                'osvdb': 0
+            },
+            {
+                'model': Vulnerability,
+                'name': 'Allowed HTTP Methods: GET, HEAD, POST, OPTIONS ',
+                'description': '[OPTIONS /] Allowed HTTP Methods: GET, HEAD, POST, OPTIONS ',
+                'severity': Severity.MEDIUM,
+                'osvdb': 0
+            },
+            {
+                'model': Vulnerability,
+                'name': 'OSVDB-3268',
+                'description': '[GET /images/] /images/: Directory indexing found.',
+                'severity': Severity.MEDIUM,
+                'osvdb': 3268
+            },
+            {'model': Endpoint, 'endpoint': '/images/'},
+            {
+                'model': Vulnerability,
+                'name': 'OSVDB-3233',
+                'description': '[GET /icons/README] /icons/README: Apache default file found.',
+                'severity': Severity.MEDIUM,
+                'osvdb': 3233
+            },
+            {'model': Endpoint, 'endpoint': '/icons/README'}
+        ]
+        super().check_tool_parser('default.xml', expected)
--- rekono/tools/tools/nikto.py
+++ rekono/tools/tools/nikto.py
@@ -13,26 +13,17 @@ class NiktoTool(BaseTool):
 
     def parse_output_file(self) -> None:
         '''Parse tool output file to create finding entities.'''
-        http_endpoints = set()                                                  # HTTP endpoints set
+        http_endpoints = set(['/'])                                             # HTTP endpoints set
         root = parser.parse(self.path_output).getroot()                         # Report root
         items = root.findall('niktoscan')[-1].findall('scandetails')[0].findall('item')     # Get report items
         for item in items:                                                      # For each item
             osvdb = int(item.attrib['osvdbid'])                                 # Get OSVDB Id
             method = item.attrib['method']                                      # Get HTTP method
-            name = None
-            description = None
-            description_tag = item.find('description')                          # Get description tag
-            if description_tag:
-                description = description_tag.text                              # Get description value
-                name = description                                              # Name initialization to description
+            description = item.findtext('description')                          # Get description value
+            name = description                                                  # Name initialization to description
             if osvdb:                                                           # If OSVDB exists
-                name = str(osvdb)                                               # Set OSCDB as name
-            endpoint = None
-            endpoint_tag = item.find('uri')                                     # Get endpoint tag
-            if endpoint_tag:
-                endpoint = endpoint_tag.text                                    # Get endpoint value
-                if endpoint and '<![DATA[' in endpoint:
-                    endpoint = endpoint.split('<![DATA[', 1)[1].rsplit(']]>', 1)[0]     # Clean endpoint value
+                name = f'OSVDB-{osvdb}'                                         # Set OSVDB as name
+            endpoint = item.findtext('uri')                                     # Get endpoint tag
             if description:
                 self.create_finding(                                            # Create Vulnerability
                     Vulnerability,
