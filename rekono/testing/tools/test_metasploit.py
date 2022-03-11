57800550893584d3349c493c0468f1c1991b6751
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-03-11 19:40:46 +0100

    Support for Metasploit

---
+++ rekono/testing/tools/test_metasploit.py
@@ -0,0 +1,38 @@
+from findings.models import Exploit
+from testing.tools.base import ToolParserTest
+
+
+class MetasploitParserTest(ToolParserTest):
+    '''Test cases for Metasploit parser.'''
+
+    tool_name = 'Metasploit'
+
+    def test_metasploit_with_exploits(self) -> None:
+        '''Test to parse report with exploits.'''
+        expected = [
+            {
+                'model': Exploit,
+                'title': 'HP Data Protector Encrypted Communication Remote Command Execution',
+                'reference': 'exploit/windows/misc/hp_dataprotector_encrypted_comms'
+            },
+            {
+                'model': Exploit,
+                'title': 'Ruby on Rails ActionPack Inline ERB Code Execution',
+                'reference': 'exploit/multi/http/rails_actionpack_inline_exec'
+            },
+            {
+                'model': Exploit,
+                'title': 'Xymon Daemon Gather Information',
+                'reference': 'auxiliary/gather/xymon_info'
+            },
+            {
+                'model': Exploit,
+                'title': 'Xymon useradm Command Execution',
+                'reference': 'exploit/unix/webapp/xymon_useradm_cmd_exec'
+            }
+        ]
+        super().check_tool_parser('exploits.txt', expected)
+
+    def test_metasploit_without_exploits(self) -> None:
+        '''Test to parse an empty report.'''
+        super().check_tool_parser('nothing.txt', [])
---
+++ rekono/tools/tools/metasploit.py
@@ -0,0 +1,16 @@
+from findings.models import Exploit
+from tools.tools.base_tool import BaseTool
+
+
+class MetasploitTool(BaseTool):
+    '''Metasploit tool class.'''
+
+    def parse_output_file(self) -> None:
+        '''Parse tool output file to create finding entities.'''
+        with open(self.path_output, 'r') as output_file:
+            entry = 0
+            for line in output_file:
+                if line.strip() and line.strip().startswith(str(entry)):
+                    entry += 1
+                    data = [i for i in line.strip().split('  ') if i]
+                    self.create_finding(Exploit, title=data[5], reference=data[1])
