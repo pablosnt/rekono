133e5d9e73e4fb9faa13fbcb753e7455e096b33f
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-03-12 13:33:57 +0100

    Use plain output for Metasploit instead of output file

--- rekono/testing/tools/test_log4j_scanner.py
+++ rekono/testing/tools/test_log4j_scanner.py
@@ -1,11 +1,21 @@
 from findings.models import Vulnerability
 from testing.tools.base import ToolParserTest
+from tools.exceptions import ToolExecutionException
 
 
 class Log4jScannerParserTest(ToolParserTest):
     '''Test cases for Log4j Scanner parser.'''
 
     tool_name = 'Log4j Scanner'
+
+    def test_check_installation(self) -> None:
+        '''Test check installatino feature for Log4j Scanner.'''
+        installed = True
+        try:
+            self.tool.check_installation()
+        except ToolExecutionException:
+            installed = False
+        self.assertFalse(installed)
 
     def test_cve_2021_44228(self) -> None:
         '''Test to parse report with CVE-2021-44228 vulnerability.'''
--- rekono/tools/tools/metasploit.py
+++ rekono/tools/tools/metasploit.py
@@ -5,12 +5,15 @@ from tools.tools.base_tool import BaseTool
 class MetasploitTool(BaseTool):
     '''Metasploit tool class.'''
 
-    def parse_output_file(self) -> None:
-        '''Parse tool output file to create finding entities.'''
-        with open(self.path_output, 'r') as output_file:
-            entry = 0
-            for line in output_file:
-                if line.strip() and line.strip().startswith(str(entry)):
-                    entry += 1
-                    data = [i.strip() for i in line.strip().split('  ') if i]
-                    self.create_finding(Exploit, title=data[-1], reference=data[1])
+    def parse_plain_output(self, output: str) -> None:
+        '''Parse tool plain output to create finding entities. This should be implemented by child tool classes.
+
+        Args:
+            output (str): Plain tool output
+        '''
+        entry = 0
+        for line in output.split('\n'):                                         # Get output by lines
+            if line.strip() and line.strip().startswith(str(entry)):            # Expected line
+                entry += 1
+                data = [i.strip() for i in line.strip().split('  ') if i]       # Clean line
+                self.create_finding(Exploit, title=data[-1], reference=data[1])
