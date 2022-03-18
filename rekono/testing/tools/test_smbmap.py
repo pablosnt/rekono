1fee26fbb2d127160587ff82d70fb5300d0f49ab
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-03-18 19:20:49 +0100

    Unit tests for smbmap

---
+++ rekono/testing/tools/test_smbmap.py
@@ -0,0 +1,21 @@
+from findings.models import Endpoint
+from testing.tools.base import ToolParserTest
+
+
+class SmbmapParserTest(ToolParserTest):
+    '''Test cases for Smbmap parser.'''
+
+    tool_name = 'smbmap'
+
+    expected_shares = [
+        {'model': Endpoint, 'endpoint': 'shared', 'extra': 'READ, WRITE'},
+        {'model': Endpoint, 'endpoint': 'IPC$', 'extra': '[NO ACCESS] IPC Service (Samba 4.5.4)'}
+    ]
+
+    def test_smbmap_only_with_shares(self) -> None:
+        '''Test to parse report only with shares.'''
+        self.check_tool_output_parser('shares.txt', self.expected_shares)
+
+    def test_smbmap_with_directories(self) -> None:
+        '''Test to parse report with shares and directories.'''
+        self.check_tool_output_parser('directories.txt', self.expected_shares)
