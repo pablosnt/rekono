5f27af14f1e65d4a9a1323465ffb12fafe35196b
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-04-13 11:26:24 +0200

    Fix unescaped characters in Telegram markdown messages

--- rekono/telegram_bot/messages/ask.py
+++ rekono/telegram_bot/messages/ask.py
@@ -1,16 +1,18 @@
 '''Telegram Bot messages that ask the user to choose one option.'''
 
 ASK_FOR_PROJECT = 'Choose project'
-NO_PROJECTS = 'You have no projects. Go to Rekono to create one or ask your administrator for assign you to existing one'       # noqa: E501
+NO_PROJECTS = (
+    'You have no projects\. Go to Rekono to create one or ask your administrator for assign you to existing one'
+)
 
 ASK_FOR_TARGET = 'Choose target'
-NO_TARGETS = 'There are no targets in this project. Use the command /newtarget'
+NO_TARGETS = 'There are no targets in this project\. Use the command /newtarget'
 
 ASK_FOR_TARGET_PORT = 'Choose target port'
-NO_TARGET_PORTS = 'There are no target ports in this target. Use the command /newtargetport'
+NO_TARGET_PORTS = 'There are no target ports in this target\. Use the command /newtargetport'
 
 ASK_FOR_PROCESS = 'Choose process'
-NO_PROCESSES = 'There are no processes. Go to Rekono to create one'
+NO_PROCESSES = 'There are no processes\. Go to Rekono to create one'
 
 ASK_FOR_TOOL = 'Choose tool'
 ASK_FOR_CONFIGURATION = 'Choose configuration'
