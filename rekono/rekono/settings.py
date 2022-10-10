11d9bfe0cd76b83dad63d60fe0680fac704ac879
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-10-10 20:15:09 +0200

    Remove support for static files in API Rest

--- rekono/rekono/settings.py
+++ rekono/rekono/settings.py
@@ -83,7 +83,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django_filters',
     'taggit',
     'django_rq',
@@ -445,15 +444,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(REKONO_HOME, 'static')
-
-if not os.path.isdir(STATIC_ROOT):
-    os.mkdir(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
