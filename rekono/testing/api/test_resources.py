aa1eea2e5edf042cfde6c919acd06fff53862836
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-10-27 14:55:00 +0200

    Fix unit tests

--- rekono/testing/api/test_resources.py
+++ rekono/testing/api/test_resources.py
@@ -3,6 +3,7 @@ from typing import Any, Dict
 
 from resources.models import Wordlist
 from security.file_upload import check_checksum
+from system.models import System
 from testing.api.base import RekonoTestCase
 
 
@@ -62,6 +63,9 @@ class WordlistsTest(RekonoTestCase):
 
     def test_invalid_create(self) -> None:
         '''Test wordlist creation feature with invalid data.'''
+        system = System.objects.first()
+        system.upload_files_max_mb = 1                                          # Reduce max size allowed
+        system.save(update_fields=['upload_files_max_mb'])
         for name, file in [
             (self.name, self.passwords),                                        # Wordlist already exists
             ('Invalid size', self.invalid_size),                                # Invalid file size
