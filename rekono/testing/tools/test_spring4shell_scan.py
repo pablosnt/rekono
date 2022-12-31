616523f1181447eea44c0d9f36e05719fd09c2ee
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-12-31 15:35:40 +0100

    Remove unneeded check installation tests

--- rekono/testing/tools/test_spring4shell_scan.py
+++ rekono/testing/tools/test_spring4shell_scan.py
@@ -1,22 +1,11 @@
 from findings.models import Vulnerability
 from testing.tools.base import ToolParserTest
 
-from tools.exceptions import ToolExecutionException
-
 
 class Spring4ShellScanParserTest(ToolParserTest):
     '''Test cases for Spring4Shell Scan parser.'''
 
     tool_name = 'Spring4Shell Scan'
-
-    def test_check_installation(self) -> None:
-        '''Test check installation feature for Spring4Shell Scan.'''
-        installed = True
-        try:
-            self.tool.check_installation()
-        except ToolExecutionException:
-            installed = False
-        self.assertFalse(installed)
 
     def test_cve_2022_22963(self) -> None:
         '''Test to parse report with CVE-2022-22963 vulnerability.'''
