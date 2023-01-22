873a02761e4cffc85a482556e4cb992c000fd3ef
Pablo Santiago <pablosantiagolopez1@gmail.com>
2023-01-22 23:53:59 +0100

    Fix links to swagger and to dashboard

--- rekono/frontend/src/common/MainHeader.vue
+++ rekono/frontend/src/common/MainHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar type="dark" variant="dark">
-      <b-navbar-brand href="/" style="margin-left: 15px">
+      <b-navbar-brand to="/dashboard" style="margin-left: 15px">
         <img src="/static/logo-white.png" width="100" class="d-inline-block align-top" alt="Rekono">
       </b-navbar-brand>
       <b-navbar-nav class="ml-auto">
@@ -17,7 +17,7 @@
             <b-icon variant="black" icon="sliders"/>
             <label class="ml-2">Settings</label>
           </b-dropdown-item>
-          <b-dropdown-item :href="getUrl('/api/schema/swagger-ui.html?docExpansion=none')" target="_blank">
+          <b-dropdown-item :href="getUrl('/api/schema/swagger-ui.html')" target="_blank">
             <b-icon variant="danger" icon="code-slash"/>
             <label class="ml-2">Rekono API Rest</label>
           </b-dropdown-item>
