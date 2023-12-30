0c4839ab63784d6dd11950cbf527d23a723977cb
Pablo Santiago <pablosantiagolopez1@gmail.com>
2023-12-30 13:42:59 +0100

    Fix typing

--- src/backend/executions/views.py
+++ src/backend/executions/views.py
@@ -64,7 +64,7 @@ class ExecutionViewSet(BaseViewSet):
             return Response(
                 {"execution": "Execution is not completed"}, status=HTTP_400_BAD_REQUEST
             )
-        path = Path(CONFIG.reports) / execution.output_file or ""
+        path = Path(CONFIG.reports) / (execution.output_file or "")
         if not execution.output_file or not path.is_file():
             return Response(status=HTTP_404_NOT_FOUND)
         return FileResponse(
