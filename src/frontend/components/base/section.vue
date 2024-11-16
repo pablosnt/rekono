3b3d21265f19b1a7af42fc6cd364a3f389aaf8af
Pablo Santiago <pablosantiagolopez1@gmail.com>
2024-11-16 09:01:43 +0100

    Optimize administration settings page

---
+++ src/frontend/components/base/section.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card variant="text" width="100%" :subtitle="subtitle">
+    <template #prepend>
+      <v-card-title v-if="icon" :class="titleClass">
+        <v-icon :color="color" :icon="icon" />
+      </v-card-title>
+    </template>
+    <template #title>
+      <v-card-title :class="titleClass">{{ title }}</v-card-title>
+    </template>
+    <template #text>
+      <v-card-text>
+        <slot />
+      </v-card-text>
+    </template>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: String,
+  titleClass: {
+    type: String,
+    required: false,
+    default: "text-h5",
+  },
+  subtitle: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+  icon: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+  color: {
+    type: String,
+    required: false,
+    default: "red",
+  },
+});
+</script>
--- src/frontend/pages/administration/settings.vue
+++ src/frontend/pages/administration/settings.vue
@@ -2,131 +2,86 @@
   <MenuAdministration>
     <v-form v-model="valid" @submit.prevent="submit()">
       <v-container fluid>
-        <v-row justify="center" dense>
+        <v-row justify="space-around" dense>
           <v-col cols="5">
-            <v-card variant="text" class="mt-5" width="100%">
-              <template #title>
-                <v-card-title class="text-h5">
-                  <v-icon color="red" icon="mdi-bug" />
-                  <span class="me-1" />
-                  Vulnerability Management
-                </v-card-title>
-              </template>
-              <template #text>
-                <v-card-text>
-                  <v-switch
-                    v-model="settings.auto_fix_findings"
-                    color="red"
-                    label="Mark findings as fixed automatically when they are no longer detected by the same tool and configuration"
-                    @update:model-value="disabled = false"
-                  />
-                </v-card-text>
-              </template>
-            </v-card>
+            <BaseSection
+              class="mt-5"
+              title="Vulnerability Management"
+              icon="mdi-bug"
+            >
+              <v-switch
+                v-model="settings.auto_fix_findings"
+                color="red"
+                label="Mark findings as fixed automatically when they are no longer detected"
+                @update:model-value="disabled = false"
+              />
+            </BaseSection>
           </v-col>
           <v-col cols="4">
-            <v-card variant="text" class="mt-5" width="100%">
-              <template #title>
-                <v-card-title class="text-h5">
-                  <v-icon color="red" icon="mdi-lock" />
-                  <span class="me-1" />
-                  Security
-                </v-card-title>
-              </template>
-              <template #text>
-                <v-card-text>
-                  <v-container fluid>
-                    <v-row justify="center" dense>
-                      <v-col cols="8">
-                        <VNumberInput
-                          v-model="settings.max_uploaded_file_mb"
-                          control-variant="split"
-                          label="Max Upload File Size (MB)"
-                          inset
-                          variant="outlined"
-                          :max="3072"
-                          :min="128"
-                          @update:model-value="disabled = false"
-                        />
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-              </template>
-            </v-card>
+            <BaseSection class="mt-5" title="Security" icon="mdi-lock">
+              <div class="text-center mr-10 ml-10">
+                <VNumberInput
+                  v-model="settings.max_uploaded_file_mb"
+                  control-variant="split"
+                  label="Max Upload File Size (MB)"
+                  inset
+                  variant="outlined"
+                  :max="3072"
+                  :min="128"
+                  @update:model-value="disabled = false"
+                />
+              </div>
+            </BaseSection>
           </v-col>
         </v-row>
         <v-row justify="center" dense>
-          <v-col cols="9">
-            <v-card variant="text" class="mt-5" width="100%">
-              <template #title>
-                <v-card-title class="text-h5">
-                  <v-icon color="red" icon="mdi-plus-network" />
-                  <span class="me-1" />
-                  Proxy
-                </v-card-title>
-              </template>
-              <template #text>
-                <v-card-text>
-                  <v-container fluid>
-                    <v-row justify="space-around" dense>
-                      <v-col cols="8">
-                        <v-alert
-                          color="info"
-                          icon="$info"
-                          variant="tonal"
-                          text="Proxies only will be used during tool executions by configuring the following environment variables"
-                        />
-                      </v-col>
-                      <v-col
-                        v-for="name in [
-                          'all_proxy',
-                          'no_proxy',
-                          'http_proxy',
-                          'https_proxy',
-                          'ftp_proxy',
-                        ]"
-                        :key="name"
-                        cols="5"
-                      >
-                        <v-text-field
-                          :v-model="settings[name]"
-                          class="mt-2"
-                          variant="underlined"
-                          :label="name.toUpperCase()"
-                          :rules="[
-                            (p) =>
-                              !p ||
-                              validate.targetRegex.test(p.trim()) ||
-                              'Invalid proxy value',
-                          ]"
-                          validate-on="input"
-                          @update:model-value="disabled = false"
-                        >
-                          <template #prepend>
-                            <p class="text-medium-emphasis">
-                              export {{ name.toUpperCase() }} =
-                            </p>
-                          </template>
-                          <template #prepend-inner> " </template>
-                          <template #append-inner> " </template>
-                        </v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-              </template>
-            </v-card>
+          <v-col cols="10">
+            <BaseSection
+              class="mt-5"
+              title="Proxy"
+              subtitle="Proxies will be used during tool executions by configuring the following environment variables"
+              icon="mdi-plus-network"
+            >
+              <v-container fluid>
+                <v-row justify="space-around" dense>
+                  <v-col
+                    v-for="name in [
+                      'all_proxy',
+                      'no_proxy',
+                      'http_proxy',
+                      'https_proxy',
+                      'ftp_proxy',
+                    ]"
+                    :key="name"
+                    cols="5"
+                  >
+                    <v-text-field
+                      :v-model="settings[name]"
+                      class="mt-2"
+                      variant="outlined"
+                      :label="name.toUpperCase()"
+                      :rules="[
+                        (p) =>
+                          !p ||
+                          validate.targetRegex.test(p.trim()) ||
+                          'Invalid proxy value',
+                      ]"
+                      validate-on="input"
+                      @update:model-value="disabled = false"
+                    />
+                  </v-col>
+                </v-row>
+              </v-container>
+            </BaseSection>
           </v-col>
         </v-row>
         <v-fab
           color="red"
           icon
-          class="position-fixed"
-          location="bottom"
+          absolute
           size="64"
+          :loading="loading"
           :disabled="disabled"
-          app
           @click="submit()"
         >
           <v-icon icon="mdi-floppy" />
@@ -144,6 +99,7 @@ const validate = useValidation();
 const valid = ref(true);
 const api = useApi("/api/settings/", true, "Configuration");
 const disabled = ref(false);
+const loading = ref(false);
 const settings = ref({
   auto_fix_findings: true,
   max_uploaded_file_mb: 128,
@@ -160,10 +116,17 @@ api.get(1).then((response) => {
 
 function submit(): void {
   if (valid.value) {
-    api.update(settings.value, 1).then((response) => {
-      settings.value = response;
-      disabled.value = true;
-    });
+    loading.value = true;
+    api
+      .update(settings.value, 1)
+      .then((response) => {
+        settings.value = response;
+        disabled.value = true;
+        loading.value = false;
+      })
+      .catch(() => {
+        loading.value = false;
+      });
   }
 }
 </script>
