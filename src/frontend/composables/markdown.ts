7ed18eac5dd9eedde2e051a6fca05f247775b4ba
Pablo Santiago <69458381+pablosnt@users.noreply.github.com>
2025-05-10 15:41:49 +0000

    Fix eslint ignore

--- src/frontend/composables/markdown.ts
+++ src/frontend/composables/markdown.ts
@@ -17,7 +17,8 @@ export function useMarkdown() {
         } else {
           result = hljs.highlightAuto(str).value;
         }
-      } catch (__) {} // eslint-disable-line no-empty
+      /* eslint-disable-next-line no-empty, @typescript-eslint/no-unused-vars */
+      } catch (__) {}
       if (result.length > 0) {
         result = '<pre><code class="hljs">' + result + "</code></pre>";
       }
