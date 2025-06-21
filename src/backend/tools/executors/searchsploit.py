a8c648e39d7eb9714c5c154e597a53b10490f50d
Pablo Santiago <pablosantiagolopez1@gmail.com>
2025-06-21 12:32:57 +0200

    New configuration for searchsploit based on CVEs

---
+++ src/backend/tools/executors/searchsploit.py
@@ -0,0 +1,22 @@
+from findings.models import Finding
+from parameters.models import InputTechnology, InputVulnerability
+from target_ports.models import TargetPort
+from tools.executors.base import BaseExecutor
+from wordlists.models import Wordlist
+
+
+class Searchsploit(BaseExecutor):
+    def _get_arguments(
+        self,
+        findings: list[Finding],
+        target_ports: list[TargetPort],
+        input_vulnerabilities: list[InputVulnerability],
+        input_technologies: list[InputTechnology],
+        wordlists: list[Wordlist],
+    ) -> list[str]:
+        arguments = super()._get_arguments(findings, target_ports, input_vulnerabilities, input_technologies, wordlists)
+        if "--cve" not in arguments and len(arguments) == 3:
+            raise RuntimeError(
+                f"Argument 'technology' or 'cve' is required to execute tool '{self.execution.configuration.tool.name}'"
+            )
+        return arguments
