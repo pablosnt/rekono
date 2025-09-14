5f4eeed500d15bf9438d7cb5978c0aa956b1fdd4
Pablo Santiago <pablosantiagolopez1@gmail.com>
2025-09-14 14:15:10 +0200

    Unit tests for triaging stats

--- src/backend/tests/stats/test_triaging.py
+++ src/backend/tests/stats/test_triaging.py
@@ -1,14 +1,63 @@
 from executions.models import Execution
 from findings.enums import TriageStatus
 from findings.models import Credential, Vulnerability
-from tests.framework import StatsTest
+from tests.framework import ApiTest, StatsTest
 from tests.framework.cases import ApiTestCase
 from tests.framework.data import SetupProject
 
 
 class TriagingStatsTest(StatsTest):
     endpoint = "/api/stats/triaging/"
-    data = [SetupProject()]
+    data = [
+        SetupProject(
+            osint_fields=[{"triage_status": TriageStatus.TRUE_POSITIVE}, {"is_fixed": True}, {}],
+            credentials_fields=[{"triage_status": TriageStatus.WONT_FIX}, {"is_fixed": True}],
+            vulnerabilities_fields=[
+                {"triage_status": TriageStatus.TRUE_POSITIVE},
+                {"triage_status": TriageStatus.FALSE_POSITIVE},
+                {},
+            ],
+            exploits_fields=[{"triage_status": TriageStatus.WONT_FIX}],
+        ),
+        SetupProject(
+            osint_fields=[],
+            credentials_fields=[],
+            vulnerabilities_fields=[
+                {"triage_status": TriageStatus.TRUE_POSITIVE, "is_fixed": True},
+                {"triage_status": TriageStatus.FALSE_POSITIVE},
+            ],
+            exploits_fields=[],
+        ),
+    ]
     cases = [
-        ApiTestCase(["members"], expected=[{"triage_status": TriageStatus.UNTRIAGED.value, "open": 4, "fixed": 0}])
+        ApiTestCase(
+            ["members"],
+            expected=[
+                {"triage_status": TriageStatus.FALSE_POSITIVE.value, "open": 2, "fixed": 0},
+                {"triage_status": TriageStatus.TRUE_POSITIVE.value, "open": 2, "fixed": 1},
+                {"triage_status": TriageStatus.UNTRIAGED.value, "open": 2, "fixed": 2},
+                {"triage_status": TriageStatus.WONT_FIX.value, "open": 4, "fixed": 0},
+            ],
+        ),
+        ApiTestCase(["not_members"]),
+        ApiTestCase(
+            ["members"],
+            expected=[
+                {"triage_status": TriageStatus.FALSE_POSITIVE.value, "open": 1, "fixed": 0},
+                {"triage_status": TriageStatus.TRUE_POSITIVE.value, "open": 2, "fixed": 0},
+                {"triage_status": TriageStatus.UNTRIAGED.value, "open": 2, "fixed": 2},
+                {"triage_status": TriageStatus.WONT_FIX.value, "open": 4, "fixed": 0},
+            ],
+            endpoint="{endpoint}?project=1",
+        ),
+        ApiTestCase(["not_members"], endpoint="{endpoint}?project=2"),
+        ApiTestCase(
+            ["members"],
+            expected=[
+                {"triage_status": TriageStatus.FALSE_POSITIVE.value, "open": 1, "fixed": 0},
+                {"triage_status": TriageStatus.TRUE_POSITIVE.value, "open": 0, "fixed": 1},
+            ],
+            endpoint="{endpoint}?target=2",
+        ),
+        ApiTestCase(["not_members"], endpoint="{endpoint}?target=1"),
     ]
