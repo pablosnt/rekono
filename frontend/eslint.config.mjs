86aa87ec4e196fd7f4046c7037e2f3f80f44ad6f
Pablo Santiago <pablosantiagolopez1@gmail.com>
2025-12-16 19:58:47 +0100

    Fix incompatibilities between eslint and prettier

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default withNuxt({
   rules: {
     "vue/multi-word-component-names": "off",
     "vue/require-default-prop": "off",
   },
-});
+}).append(eslintConfigPrettier);
 // Your custom configs here
