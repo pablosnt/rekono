9ca8a6fcaf88b3599e65ddc76756b78f761c52ce
Pablo Santiago <pablosantiagolopez1@gmail.com>
2026-04-20 19:00:27 +0200

    Path page

---
+++ frontend/app/pages/projects/[project_id]/paths/[path_id].vue
@@ -0,0 +1,46 @@
+<template>
+  <FindingsSingle
+    v-if="path"
+    :icon="pathType.icon"
+    :api="api"
+    :title="path.path"
+    :finding="path"
+    entity-name="Path"
+    is-asset
+    fix-verb="Discard"
+    @update="fetch()"
+  >
+    <template #metadata>
+      <FindingsMetadataHost :host="path.port?.host" />
+      <FindingsMetadataPort :port="path.port" />
+      <div v-if="path.type" class="flex items-center gap-2">
+        <span class="text-muted">Type:</span>
+        <span class="text-base">{{ path.type }}</span>
+      </div>
+      <div v-if="path.status" class="flex items-center gap-2">
+        <span class="text-muted">Status:</span>
+        <span class="text-base">{{ path.status }}</span>
+      </div>
+    </template>
+  </FindingsSingle>
+</template>
+
+<script setup lang="ts">
+import { pathTypes } from "~/constants";
+
+definePageMeta({ layout: "project" });
+
+const api = useApi("/api/paths/");
+const route = useRoute();
+const path = ref();
+const pathType = ref();
+
+function fetch() {
+  api.get(`${route.params.path_id}/`).then((response) => {
+    path.value = response;
+    pathType.value = pathTypes.find((t) => t.value === response.type);
+  });
+}
+
+onMounted(fetch);
+</script>
