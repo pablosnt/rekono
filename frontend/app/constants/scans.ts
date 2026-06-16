a27471bcbfb83bd5ce99a475559d8e01ffdd7197
Pablo Santiago <pablosantiagolopez1@gmail.com>
2026-06-16 18:02:35 +0200

    Exclude 'Vulnerabilities' stage from selectors as there are no configurations associated to it

--- frontend/app/constants/scans.ts
+++ frontend/app/constants/scans.ts
@@ -1,7 +1,7 @@
 export const stages = [
   { label: "OSINT", value: 1, icon: "i-lucide-rss" },
   { label: "Enumeration", value: 2, icon: "i-lucide-network" },
-  { label: "Vulnerabilities", value: 3, icon: "i-lucide-bug" },
+  // { label: "Vulnerabilities", value: 3, icon: "i-lucide-bug" },
   { label: "Services", value: 4, icon: "i-lucide-server" },
   { label: "Exploitation", value: 5, icon: "i-lucide-flame" },
 ];
