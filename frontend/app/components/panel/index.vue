5fc4a4080ed0e190295843d3e1624a29213843c3
Pablo Santiago <pablosantiagolopez1@gmail.com>
2026-07-14 18:11:17 +0200

    Improve size of profile's avatar

--- frontend/app/components/panel/index.vue
+++ frontend/app/components/panel/index.vue
@@ -56,7 +56,7 @@
                 : '',
               class: 'bg-primary-500',
               ui: { fallback: 'text-white' },
-              size: 'sm',
+              size: 'md',
             }"
             :label="open ? userStore.name || undefined : undefined"
             :aria-label="
