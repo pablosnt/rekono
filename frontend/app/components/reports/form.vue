7ae6fbf514ed54d47a71861fd0d74e7306646363
Pablo Santiago <pablosantiagolopez1@gmail.com>
2026-07-17 13:28:39 +0200

    Set PDF format as default format for report creation

--- frontend/app/components/reports/form.vue
+++ frontend/app/components/reports/form.vue
@@ -141,7 +141,7 @@ const formData = ref<Record<string, unknown>>({
   project: parseInt(route.params.project_id),
   target: props.entity ? props.entity.target : undefined,
   task: props.entity ? props.entity.task : undefined,
-  format: "json",
+  format: "pdf",
   only_true_positives: false,
   include_findings_from_user_input: false,
   finding_types: findingTypes.map((t) => t.value),
