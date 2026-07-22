97a26a0e12297202fdad1de2ff39e4271a260006
Pablo Santiago <pablosantiagolopez1@gmail.com>
2026-07-22 13:58:56 +0200

    Prevent clipboard undefined errors

--- frontend/app/utils/text.ts
+++ frontend/app/utils/text.ts
@@ -65,13 +65,13 @@ export function smartLowerCase(text: string): string {
 export function copyLink() {
   const url = useRequestURL();
   const toast = useToast();
-  navigator.clipboard.writeText(`${url.origin}${url.pathname}`);
+  navigator.clipboard?.writeText(`${url.origin}${url.pathname}`);
   toast.add({ title: "Link copied to clipboard", color: "success" });
 }
 
 export function copyText(text: string, message?: string) {
   const toast = useToast();
-  navigator.clipboard.writeText(text);
+  navigator.clipboard?.writeText(text);
   toast.add({
     title: message || `${text} copied to clipboard`,
     color: "success",
