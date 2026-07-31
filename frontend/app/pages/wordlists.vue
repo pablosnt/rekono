d1f0c7cf7a30df2dfbb801ee5f3262131f8b5c9b
Pablo Santiago <pablosantiagolopez1@gmail.com>
2026-07-31 09:21:23 +0200

    Fix code style

--- frontend/app/pages/wordlists.vue
+++ frontend/app/pages/wordlists.vue
@@ -153,7 +153,7 @@ const config: CrudConfig<Wordlist> = reactive({
   ordering: [
     "id",
     "name",
-    {id: "size", label: "Words"},
+    { id: "size", label: "Words" },
     "type",
     "owner",
     { id: "likes", label: "Likes" },
